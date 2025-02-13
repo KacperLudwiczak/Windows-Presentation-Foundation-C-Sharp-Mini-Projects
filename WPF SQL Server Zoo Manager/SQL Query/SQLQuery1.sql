ca137bc4b6ce1ad29e312ed9a92b4c910ecd1fb1
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-13 13:28:21 +0100

    Add WPF SQL Server Zoo Manager

---
+++ WPF SQL Server Zoo Manager/SQL Query/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿--SELECT * FROM Animal
+--SELECT * FROM ZOO
+
+-- Show all animals that are in Tokyo
+SELECT A.Name FROM Animal A INNER JOIN ZooAnimal ZA ON A.Id = ZA.AnimalId WHERE ZA.ZooId = 1
+
+SELECT * FROM Animal A INNER JOIN ZooAnimal ZA ON A.Id = ZA.AnimalId WHERE ZA.ZooId = 1
